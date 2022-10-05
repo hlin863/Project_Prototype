02515e04b66709ce7db5a337624ace33f4841a5d
hlin863 <zcabhli@ucl.ac.uk>
2022-10-05 10:59:14 +0100

    Update calculate.py
    I used Linter to correct the code syntactical errors.

--- calculate.py
+++ calculate.py
@@ -7,17 +7,21 @@ A prototype file containing the basic arithmetic functions.
 
 """
 
+
 def add(a, b):
     """Add Function"""
     return a + b
+
 
 def subtract(a, b):
     """Subtract Function"""
     return a - b
 
+
 def multiply(a, b):
     """Multiply Function"""
     return a * b
+
 
 def divide(a, b):
     """Divide Function"""
