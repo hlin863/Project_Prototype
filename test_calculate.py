29c3abdb4cbc79b59952dbb124dde074e5e1e014
hlin863 <zcabhli@ucl.ac.uk>
2022-10-05 12:18:18 +0100

    Update test_calculate.py
    I removed whitespaces

--- test_calculate.py
+++ test_calculate.py
@@ -13,11 +13,11 @@ class Test:
     def test_add(self):
         """Test the add function"""
         assert calculate.add(2, 3) == 5
-    
+
     def test_subtract(self):
         """Test the subtract function"""
         assert calculate.subtract(5, 3) == 2
-    
+
     def test_multiply(self):
         """Test the multiply function"""
         assert calculate.multiply(5, 3) == 15
